b731286ea1e24c6e0861ed84324d4109f8e81771
Julio Marques <julio.marques.cesar@gmail.com>
2022-07-12 17:26:14 -0300

    Inclussao do ShyniAppteste

--- MyShinyTeste/ui.R
+++ MyShinyTeste/ui.R
@@ -17,18 +17,20 @@ ui <- fluidPage(
     sidebarPanel(
       numericInput("numeric","Qual a quantidade de randerização no gráfico?",
                    value=nrow(mtcars),min=1,max=nrow(mtcars),step=1),
-      sliderInput("sliderx","width minimum e maximum X Values",
+      sliderInput("sliderx","Largura mínimo e máximo para eixo X",
                   min = 100, max = 550, value = 250),
-      sliderInput("slidery","height minimum e maximum y Values",
+      sliderInput("slidery","Altura mínimo e máximo para eixo Y",
                   min = 100,max = 415,value = 250),
       checkboxInput("show_title","Show Hide Title")
     ),
     # Show a plot of the generated distribution
     mainPanel(
       h3("Graph of Random Points"),
-      plotOutput("plot", brush = "plot_brush"),
-      tableOutput("data")
+      plotOutput("plot", brush = "plot_brush")
     )
-  )
+  ),
+  
+  tableOutput("data")
+
 )
 
